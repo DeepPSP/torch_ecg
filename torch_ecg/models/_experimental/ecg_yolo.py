5940f9e7f8e6db994730dd1d5ae5c03e90a31e25
WEN Hao <wenh06@gmail.com>
2020-10-08 18:02:28 +0800

    init ECG_YOLO

--- torch_ecg/models/_experimental/ecg_yolo.py
+++ torch_ecg/models/_experimental/ecg_yolo.py
@@ -1,5 +1,6 @@
 """
 3rd place (entry 0436) of CPSC2019
+and variations
 """
 
 import sys
@@ -21,14 +22,91 @@ from ...utils.misc import dict_to_str
 from ..nets import (
     Conv_Bn_Activation,
     DownSample, ZeroPadding,
+    GlobalContextBlock,
 )
 
 if Cfg.torch_dtype.lower() == 'double':
     torch.set_default_tensor_type(torch.DoubleTensor)
 
 
+__all__ = [
+    "ECG_YOLO",
+]
+
+
+class ResNetGCBlock(nn.Module):
+    """ NOT finished, NOT checked,
+
+    ResNet block with global context
+
+    References:
+    -----------
+    [1] entry 0436 of CPSC2019
+    [2] https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+    """
+    __DEBUG__ = True
+    __name__ = "ResNetGCBlock"
+
+    def __init__(self,) -> NoReturn:
+        """
+        """
+        super().__init__()
+        raise NotImplementedError
+
+    def forward(self,):
+        """
+        """
+        raise NotImplementedError
+
+    def compute_output_shape(self,):
+        """
+        """
+        raise NotImplementedError
+
+
+class StageModule(nn.Module):
+    """ NOT finished, NOT checked,
+    """
+    __DEBUG__ = True
+    __name__ = "StageModule"
+
+    def __init__(self,) -> NoReturn:
+        """
+        """
+        super().__init__()
+        raise NotImplementedError
+
+    def forward(self,):
+        """
+        """
+        raise NotImplementedError
+
+    def compute_output_shape(self,):
+        """
+        """
+        raise NotImplementedError
 
 
 
+class ECG_YOLO(nn.Module):
+    """ NOT finished, NOT checked,
 
+    """
+    __DEBUG__ = True
+    __name__ = "ECG_YOLO"
 
+    def __init__(self,) -> NoReturn:
+        """
+        """
+        super().__init__()
+        raise NotImplementedError
+
+    def forward(self,):
+        """
+        """
+        raise NotImplementedError
+
+    def compute_output_shape(self,):
+        """
+        """
+        raise NotImplementedError
