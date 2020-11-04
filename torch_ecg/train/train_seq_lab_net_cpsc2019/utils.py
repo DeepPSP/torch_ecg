8701a1938ee0c4746e34e962639a314fd4589429
WEN Hao <wenh06@gmail.com>
2020-11-04 14:15:29 +0800

    add utils for cpsc2019

---
+++ torch_ecg/train/train_seq_lab_net_cpsc2019/utils.py
@@ -0,0 +1,392 @@
+"""
+"""
+import os, sys, re, logging
+import time, datetime
+from functools import reduce
+from copy import deepcopy
+from itertools import repeat
+from numbers import Real, Number
+from typing import Union, Optional, List, Tuple, Dict, Sequence, NoReturn
+
+import numpy as np
+import pandas as pd
+
+
+__all__ = [
+    "dict_to_str",
+    "str2bool",
+    "get_date_str",
+    "mask_to_intervals",
+    "list_sum",
+    "gen_gaussian_noise", "gen_sinusoidal_noise", "gen_baseline_wander",
+    "get_record_list_recursive3",
+    "init_logger",
+]
+
+
+def dict_to_str(d:Union[dict, list, tuple], current_depth:int=1, indent_spaces:int=4) -> str:
+    """ finished, checked,
+
+    convert a (possibly) nested dict into a `str` of json-like formatted form,
+    this nested dict might also contain lists or tuples of dict (and of str, int, etc.)
+
+    Parameters:
+    -----------
+    d: dict, or list, or tuple,
+        a (possibly) nested `dict`, or a list of `dict`
+    current_depth: int, default 1,
+        depth of `d` in the (possible) parent `dict` or `list`
+    indent_spaces: int, default 4,
+        the indent spaces of each depth
+
+    Returns:
+    --------
+    s: str,
+        the formatted string
+    """
+    assert isinstance(d, (dict, list, tuple))
+    if len(d) == 0:
+        s = f"{{}}" if isinstance(d, dict) else f"[]"
+        return s
+    # flat_types = (Number, bool, str,)
+    flat_types = (Number, bool,)
+    flat_sep = ", "
+    s = "\n"
+    unit_indent = " "*indent_spaces
+    prefix = unit_indent*current_depth
+    if isinstance(d, (list, tuple)):
+        if all([isinstance(v, flat_types) for v in d]):
+            len_per_line = 110
+            current_len = len(prefix) + 1  # + 1 for a comma 
+            val = []
+            for idx, v in enumerate(d):
+                add_v = f"\042{v}\042" if isinstance(v, str) else str(v)
+                add_len = len(add_v) + len(flat_sep)
+                if current_len + add_len > len_per_line:
+                    val = ", ".join([item for item in val])
+                    s += f"{prefix}{val},\n"
+                    val = [add_v]
+                    current_len = len(prefix) + 1 + len(add_v)
+                else:
+                    val.append(add_v)
+                    current_len += add_len
+            if len(val) > 0:
+                val = ", ".join([item for item in val])
+                s += f"{prefix}{val}\n"
+        else:
+            for v in d:
+                if isinstance(v, (dict, list, tuple)):
+                    s += f"{prefix}{dict_to_str(v, current_depth+1)}\n"
+                else:
+                    val = f"\042{v}\042" if isinstance(v, str) else v
+                    s += f"{prefix}{val}\n"
+    elif isinstance(d, dict):
+        for k, v in d.items():
+            key = f"\042{k}\042" if isinstance(k, str) else k
+            if isinstance(v, (dict, list, tuple)):
+                s += f"{prefix}{key}: {dict_to_str(v, current_depth+1)}\n"
+            else:
+                val = f"\042{v}\042" if isinstance(v, str) else v
+                s += f"{prefix}{key}: {val}\n"
+    s += unit_indent*(current_depth-1)
+    s = f"{{{s}}}" if isinstance(d, dict) else f"[{s}]"
+    return s
+
+
+def str2bool(v:Union[str, bool]) -> bool:
+    """ finished, checked,
+
+    converts a 'boolean' value possibly in the format of str to bool
+
+    Parameters:
+    -----------
+    v: str or bool,
+        the 'boolean' value
+
+    Returns:
+    --------
+    b: bool,
+        `v` in the format of bool
+
+    References:
+    -----------
+    https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+    """
+    if isinstance(v, bool):
+       b = v
+    elif v.lower() in ('yes', 'true', 't', 'y', '1'):
+        b = True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        b = False
+    else:
+        raise ValueError('Boolean value expected.')
+    return b
+
+
+def get_date_str(fmt:Optional[str]=None):
+    """
+    """
+    now = datetime.datetime.now()
+    _fmt = fmt or '%Y-%m-%d-%H-%M-%S'
+    ds = now.strftime(_fmt)
+    return ds
+
+
+def mask_to_intervals(mask:np.ndarray, vals:Optional[Union[int,Sequence[int]]]=None) -> Union[list, dict]:
+    """ finished, checked,
+
+    Parameters:
+    -----------
+    mask: ndarray,
+        1d mask
+    vals: int or sequence of int, optional,
+        values in `mask` to obtain intervals
+
+    Returns:
+    --------
+    intervals: dict or list,
+        the intervals corr. to each value in `vals` if `vals` is `None` or `Sequence`;
+        or the intervals corr. to `vals` if `vals` is int.
+        each interval is of the form `[a,b]`, left inclusive, right exclusive
+    """
+    if vals is None:
+        _vals = list(set(mask))
+    elif isinstance(vals, int):
+        _vals = [vals]
+    else:
+        _vals = vals
+    # assert set(_vals) & set(mask) == set(_vals)
+
+    intervals = {v:[] for v in _vals}
+    for v in _vals:
+        valid_inds = np.where(np.array(mask)==v)[0]
+        if len(valid_inds) == 0:
+            continue
+        split_indices = np.where(np.diff(valid_inds)>1)[0]
+        split_indices = split_indices.tolist() + (split_indices+1).tolist()
+        split_indices = sorted([0] + split_indices + [len(valid_inds)-1])
+        for idx in range(len(split_indices)//2):
+            intervals[v].append(
+                [valid_inds[split_indices[2*idx]], valid_inds[split_indices[2*idx+1]]+1]
+            )
+    
+    if isinstance(vals, int):
+        intervals = intervals[vals]
+
+    return intervals
+
+
+def list_sum(l:Sequence[list]) -> list:
+    """ finished, checked,
+    """
+    return reduce(lambda a,b: a+b, l, [])
+
+
+def gen_gaussian_noise(siglen:int, mean:Real=0, std:Real=0) -> np.ndarray:
+    """ finished, checked,
+
+    generate 1d Gaussian noise of given length, mean, and standard deviation
+
+    Parameters:
+    -----------
+    siglen: int,
+        length of the noise signal
+    mean: real number, default 0,
+        mean of the noise
+    std: real number, default 0,
+        standard deviation of the noise
+
+    Returns:
+    --------
+    gn: ndarray,
+        the gaussian noise of given length, mean, and standard deviation
+    """
+    gn = np.random.normal(mean, std, siglen)
+    return gn
+
+
+def gen_sinusoidal_noise(siglen:int, start_phase:Real, end_phase:Real, amplitude:Real, amplitude_mean:Real=0, amplitude_std:Real=0) -> np.ndarray:
+    """ finished, checked,
+
+    generate 1d sinusoidal noise of given length, amplitude, start phase, and end phase
+
+    Parameters:
+    -----------
+    siglen: int,
+        length of the (noise) signal
+    start_phase: real number,
+        start phase, with units in degrees
+    end_phase: real number,
+        end phase, with units in degrees
+    amplitude: real number,
+        amplitude of the sinusoidal curve
+    amplitude_mean: real number,
+        mean amplitude of an extra Gaussian noise
+    amplitude_std: real number, default 0,
+        standard deviation of an extra Gaussian noise
+
+    Returns:
+    --------
+    sn: ndarray,
+        the sinusoidal noise of given length, amplitude, start phase, and end phase
+    """
+    sn = np.linspace(start_phase, end_phase, siglen)
+    sn = amplitude * np.sin(np.pi * sn / 180)
+    sn += gen_gaussian_noise(siglen, amplitude_mean, amplitude_std)
+    return sn
+
+
+def gen_baseline_wander(siglen:int, fs:Real, bw_fs:Union[Real,Sequence[Real]], amplitude:Union[Real,Sequence[Real]], amplitude_mean:Real=0, amplitude_std:Real=0) -> np.ndarray:
+    """ finished, checked,
+
+    generate 1d baseline wander of given length, amplitude, and frequency
+
+    Parameters:
+    -----------
+    siglen: int,
+        length of the (noise) signal
+    fs: real number,
+        sampling frequency of the original signal
+    bw_fs: real number, or list of real numbers,
+        frequency (frequencies) of the baseline wander
+    amplitude: real number, or list of real numbers,
+        amplitude of the baseline wander (corr. to each frequency band)
+    amplitude_mean: real number, default 0,
+        mean amplitude of an extra Gaussian noise
+    amplitude_std: real number, default 0,
+        standard deviation of an extra Gaussian noise
+
+    Returns:
+    --------
+    bw: ndarray,
+        the baseline wander of given length, amplitude, frequency
+
+    Example:
+    --------
+    >>> gen_baseline_wander(4000, 400, [0.4,0.1,0.05], [0.1,0.2,0.4])
+    """
+    bw = gen_gaussian_noise(siglen, amplitude_mean, amplitude_std)
+    if isinstance(bw_fs, Real):
+        _bw_fs = [bw_fs]
+    else:
+        _bw_fs = bw_fs
+    if isinstance(amplitude, Real):
+        _amplitude = list(repeat(amplitude, len(_bw_fs)))
+    else:
+        _amplitude = amplitude
+    assert len(_bw_fs) == len(_amplitude)
+    duration = (siglen / fs)
+    for bf, a in zip(_bw_fs, _amplitude):
+        start_phase = np.random.randint(0,360)
+        end_phase = duration * bf * 360 + start_phase
+        bw += gen_sinusoidal_noise(siglen, start_phase, end_phase, a, 0, 0)
+    return bw
+
+
+def get_record_list_recursive3(db_dir:str, rec_patterns:Union[str,Dict[str,str]]) -> Union[List[str], Dict[str, List[str]]]:
+    """ finished, checked,
+
+    get the list of records in `db_dir` recursively,
+    for example, there are two folders 'patient1', 'patient2' in `db_dir`,
+    and there are records 'A0001', 'A0002', ... in 'patient1'; 'B0001', 'B0002', ... in 'patient2',
+    then the output would be 'patient1{sep}A0001', ..., 'patient2{sep}B0001', ...,
+    sep is determined by the system
+
+    Parameters:
+    -----------
+    db_dir: str,
+        the parent (root) path of the whole database
+    rec_patterns: str or dict,
+        pattern of the record filenames, e.g. "A(?:\d+).mat",
+        or patterns of several subsets, e.g. `{"A": "A(?:\d+).mat"}`
+
+    Returns:
+    --------
+    res: list of str,
+        list of records, in lexicographical order
+    """
+    if isinstance(rec_patterns, str):
+        res = []
+    elif isinstance(rec_patterns, dict):
+        res = {k:[] for k in rec_patterns.keys()}
+    db_dir = os.path.join(db_dir, "tmp").replace("tmp", "")  # make sure `db_dir` ends with a sep
+    roots = [db_dir]
+    while len(roots) > 0:
+        new_roots = []
+        for r in roots:
+            tmp = [os.path.join(r, item) for item in os.listdir(r)]
+            # res += [item for item in tmp if os.path.isfile(item)]
+            if isinstance(rec_patterns, str):
+                res += list(filter(re.compile(rec_patterns).search, tmp))
+            elif isinstance(rec_patterns, dict):
+                for k in rec_patterns.keys():
+                    res[k] += list(filter(re.compile(rec_patterns[k]).search, tmp))
+            new_roots += [item for item in tmp if os.path.isdir(item)]
+        roots = deepcopy(new_roots)
+    if isinstance(rec_patterns, str):
+        res = [os.path.splitext(item)[0].replace(db_dir, "") for item in res]
+        res = sorted(res)
+    elif isinstance(rec_patterns, dict):
+        for k in rec_patterns.keys():
+            res[k] = [os.path.splitext(item)[0].replace(db_dir, "") for item in res[k]]
+            res[k] = sorted(res[k])
+    return res
+
+
+def init_logger(log_dir:str, log_file:Optional[str]=None, mode:str='a', verbose:int=0) -> logging.Logger:
+    """ finished, checked,
+
+    Parameters:
+    -----------
+    log_dir: str,
+        directory of the log file
+    log_file: str, optional,
+        name of the log file
+    mode: str, default 'a',
+        mode of writing the log file, can be one of 'a', 'w'
+    verbose: int, default 0,
+        log verbosity
+
+    Returns:
+    --------
+    logger: Logger
+    """
+    if log_dir is None:
+        log_dir = '~/temp/log/'
+    if log_file is None:
+        log_file = f'log_{get_date_str()}.txt'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    log_file = os.path.join(log_dir, log_file)
+    print(f'log file path: {log_file}')
+
+    logger = logging.getLogger('ECG-CRNN')
+
+    c_handler = logging.StreamHandler(sys.stdout)
+    f_handler = logging.FileHandler(log_file)
+
+    if verbose >= 2:
+        print("levels of c_handler and f_handler are set DEBUG")
+        c_handler.setLevel(logging.DEBUG)
+        f_handler.setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
+    elif verbose >= 1:
+        print("level of c_handler is set INFO, level of f_handler is set DEBUG")
+        c_handler.setLevel(logging.INFO)
+        f_handler.setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
+    else:
+        print("levels of c_handler and f_handler are set WARNING")
+        c_handler.setLevel(logging.WARNING)
+        f_handler.setLevel(logging.WARNING)
+        logger.setLevel(logging.WARNING)
+
+    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+
+    return logger
