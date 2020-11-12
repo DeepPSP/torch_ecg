589333c19764630fa7756d630881c10fb84c4704
WEN Hao <wenh06@gmail.com>
2020-11-12 11:49:24 +0800

    add training notebook for cpsc2019 as an example

--- torch_ecg/__init__.py
+++ torch_ecg/__init__.py
@@ -7,6 +7,8 @@ _PARENT_DIR = os.path.dirname(_BASE_DIR)
 # _IN_SYS_PATH = [p for p in [_BASE_DIR, _PARENT_DIR] if p in sys.path]
 if _PARENT_DIR not in sys.path:
     sys.path.insert(0, _PARENT_DIR)
+if _BASE_DIR not in sys.path:
+    sys.path.insert(0, _BASE_DIR)
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
