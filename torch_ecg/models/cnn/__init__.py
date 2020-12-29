026818256040054ad47c4a18748acfade456b470
WEN Hao <wenh06@gmail.com>
2020-12-29 22:40:22 +0800

    update __init__ file of cnn

--- torch_ecg/models/cnn/__init__.py
+++ torch_ecg/models/cnn/__init__.py
@@ -8,12 +8,27 @@ named CNNs, which are frequently used by more complicated models, including
 6. to add more
 """
 
-# from .densenet import (
-#     DenseBasicBlock, DenseBottleNeck,
-#     DenseMacroBlock, DenseTransition,
-#     DenseNet,
-# )
-# from .multi_scopic import MultiScopicBasicBlock, MultiScopicBranch, MultiScopicCNN
-# from .resnet import 
-# from .vgg import VGGBlock, VGG16
+from .densenet import (
+    DenseBasicBlock, DenseBottleNeck,
+    DenseMacroBlock, DenseTransition,
+    DenseNet,
+)
+from .multi_scopic import MultiScopicBasicBlock, MultiScopicBranch, MultiScopicCNN
+from .resnet import ResNetBasicBlock, ResNetBottleNeck, ResNet
+from .vgg import VGGBlock, VGG16
 # from .xception import
+
+
+__all__ = [
+    # VGG
+    "VGGBlock", "VGG16",
+
+    # ResNet
+    "ResNetBasicBlock", "ResNetBottleNeck", "ResNet",
+
+    # MultiScopic
+    "MultiScopicBasicBlock", "MultiScopicBranch", "MultiScopicCNN",
+
+    # DenseNet
+    "DenseBasicBlock", "DenseBottleNeck", "DenseMacroBlock", "DenseTransition", "DenseNet",
+]
