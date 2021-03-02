155b6372e14a807cc54c2fc57b15f4cce9fb8bb4
WEN Hao <wenh06@gmail.com>
2021-03-02 22:05:29 +0800

    debug

--- torch_ecg/__init__.py
+++ torch_ecg/__init__.py
@@ -3,12 +3,12 @@
 import os, sys
 
 _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-_PARENT_DIR = os.path.dirname(_BASE_DIR)
+_PARENT_DIR = os.path.dirname(_BASE_DIR)  # e.g. "site-packages"
 # _IN_SYS_PATH = [p for p in [_BASE_DIR, _PARENT_DIR] if p in sys.path]
 if _PARENT_DIR not in sys.path:
     sys.path.insert(0, _PARENT_DIR)
-if _BASE_DIR not in sys.path:
-    sys.path.insert(0, _BASE_DIR)
+# if _BASE_DIR not in sys.path:
+#     sys.path.insert(0, _BASE_DIR)
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
