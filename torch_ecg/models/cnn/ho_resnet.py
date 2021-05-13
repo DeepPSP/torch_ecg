88aba74a59c051796687ac8815c55740da3d161a
WEN Hao <wenh06@gmail.com>
2021-05-13 22:32:27 +0800

    init ho-resnet

---
+++ torch_ecg/models/cnn/ho_resnet.py
@@ -0,0 +1,66 @@
+"""
+Higher Order ResNet
+
+References:
+-----------
+[1] Luo, Z., Sun, Z., Zhou, W., & Kamata, S. I. (2021). Rethinking ResNets: Improved Stacking Strategies With High Order Schemes. arXiv preprint arXiv:2103.15244.
+"""
+from copy import deepcopy
+from itertools import repeat
+from typing import Union, Optional, Sequence, NoReturn
+
+import numpy as np
+np.set_printoptions(precision=5, suppress=True)
+import torch
+from torch import nn
+from torch import Tensor
+from easydict import EasyDict as ED
+
+from ...cfg import Cfg
+from ...utils.utils_nn import compute_module_size
+from ...utils.misc import dict_to_str
+from ...models.nets import (
+    Activations,
+    Conv_Bn_Activation,
+    DownSample,
+    ZeroPadding,
+    NonLocalBlock, SEBlock, GlobalContextBlock,
+)
+
+if Cfg.torch_dtype.lower() == "double":
+    torch.set_default_tensor_type(torch.DoubleTensor)
+
+
+__all__ = [
+    "MidPointResNet",
+    "RK4ResNet",
+    "RK8ResNet",
+]
+
+
+class MidPointResNet(nn.Module):
+    """
+    """
+    def __init__(self, in_channels:int, **config) -> NoReturn:
+        """
+        """
+        raise NotImplementedError
+
+
+class RK4ResNet(nn.Module):
+    """
+    """
+    def __init__(self, in_channels:int, **config) -> NoReturn:
+        """
+        """
+        raise NotImplementedError
+
+
+class RK8ResNet(nn.Module):
+    """
+    """
+    def __init__(self, in_channels:int, **config) -> NoReturn:
+        """
+        """
+        raise NotImplementedError
+
