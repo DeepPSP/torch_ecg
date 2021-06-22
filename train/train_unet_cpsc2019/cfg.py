74b64cddb5feb1d6cc174e80bff92fd7a95c4f24
WEN Hao <wenh06@gmail.com>
2021-06-22 09:51:20 +0800

    increase tolerance of early stopping for cpsc2019

--- train/train_unet_cpsc2019/cfg.py
+++ train/train_unet_cpsc2019/cfg.py
@@ -272,7 +272,7 @@ TrainCfg.steps = [5000, 10000]
 
 TrainCfg.early_stopping = ED()  # early stopping according to challenge metric
 TrainCfg.early_stopping.min_delta = 0.001  # should be non-negative
-TrainCfg.early_stopping.patience = 6
+TrainCfg.early_stopping.patience = 15
 
 # configs of loss function
 TrainCfg.loss = "BCEWithLogitsLoss"
