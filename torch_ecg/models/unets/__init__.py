d8e9e5d6a6da4cc7eb239b38d1333a279f78e5c2
WEN Hao <wenh06@gmail.com>
2021-07-05 16:24:25 +0800

    add __init__.py for the unets submodule

---
+++ torch_ecg/models/unets/__init__.py
@@ -0,0 +1,12 @@
+"""
+Various UNets for ECG waveform segmentation
+"""
+
+from .ecg_subtract_unet import ECG_SUBTRACT_UNET  # CPSC2019 unet
+from .ecg_unet import ECG_UNET  # vanilla unet
+
+
+__all__ = [
+    "ECG_SUBTRACT_UNET",
+    "ECG_UNET",
+]
