29b868866196c889b5f8eb222cb81908e4057369
WEN Hao <wenh06@gmail.com>
2021-09-07 17:31:16 +0800

    update init of torch_ecg

--- torch_ecg/__init__.py
+++ torch_ecg/__init__.py
@@ -6,7 +6,8 @@ _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 _PARENT_DIR = os.path.dirname(_BASE_DIR)  # e.g. "site-packages"
 # _IN_SYS_PATH = [p for p in [_BASE_DIR, _PARENT_DIR] if p in sys.path]
 if _PARENT_DIR not in sys.path:
-    sys.path.insert(0, _PARENT_DIR)
+    # sys.path.insert(0, _PARENT_DIR)
+    sys.path.append(_PARENT_DIR)
 # if _BASE_DIR not in sys.path:
 #     sys.path.insert(0, _BASE_DIR)
 
