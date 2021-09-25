6ee8a95f2bef02fd5b53ec64bc531a975db71d31
WEN Hao <wenh06@gmail.com>
2021-09-25 15:44:03 +0800

    add __init__ for model_configs

--- torch_ecg/model_configs/__init__.py
+++ torch_ecg/model_configs/__init__.py
@@ -2,23 +2,34 @@
 """
 
 from .cnn import (
+    # vgg
     vgg_block_basic, vgg_block_mish, vgg_block_swish,
     vgg16, vgg16_leadwise,
+    # vanilla resnet
     resnet_vanilla_18, resnet_vanilla_34,
     resnet_vanilla_50, resnet_vanilla_101, resnet_vanilla_152,
     resnext_vanilla_50_32x4d, resnext_vanilla_101_32x8d,
     resnet_vanilla_wide_50_2, resnet_vanilla_wide_101_2,
+    # custom resnet
     resnet_block_basic, resnet_bottle_neck,
     resnet, resnet_leadwise,
+    # stanford resnet
     resnet_block_stanford, resnet_stanford,
+    # cpsc2018 SOTA
     cpsc_block_basic, cpsc_block_mish, cpsc_block_swish,
     cpsc_2018, cpsc_2018_leadwise,
+    # multi_scopic
     multi_scopic_block,
     multi_scopic, multi_scopic_leadwise,
+    # vanilla dense_net
     dense_net_vanilla,
+    # custom dense_net
     dense_net_leadwise,
+    # vanilla xception
     xception_vanilla,
+    # custom xception
     xception_leadwise,
+    # vanilla mobilenets
     mobilenet_v1_vanilla,
 )
 from .rnn import lstm, attention
