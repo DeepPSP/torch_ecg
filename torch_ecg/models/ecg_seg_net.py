2e293e25d5596019114c50dab6f7fdd1aa7102f3
WEN Hao <wenh06@gmail.com>
2021-10-24 10:47:31 +0800

    init segmentation networks

---
+++ torch_ecg/models/ecg_seg_net.py
@@ -0,0 +1,21 @@
+"""
+Segmentation using Fully-Convolutional Network
+
+References
+----------
+[1] https://github.com/pytorch/vision/tree/master/torchvision/models/segmentation
+"""
+from copy import deepcopy
+from itertools import repeat
+from collections import OrderedDict
+from typing import Union, Optional, Tuple, List, Sequence, NoReturn, Any
+from numbers import Real, Number
+
+import numpy as np
+np.set_printoptions(precision=5, suppress=True)
+import pandas as pd
+import torch
+from torch import nn
+from torch import Tensor
+import torch.nn.functional as F
+from easydict import EasyDict as ED
