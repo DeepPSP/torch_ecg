0d339b178ae4e18b199132f8af8f7f35f487a419
WEN Hao <wenh06@gmail.com>
2021-11-09 21:25:07 +0800

    add class helper function default_class_repr

--- torch_ecg/utils/misc.py
+++ torch_ecg/utils/misc.py
@@ -43,6 +43,7 @@ __all__ = [
     "list_sum",
     "read_log_txt", "read_event_scalars",
     "dicts_equal",
+    "default_class_repr",
 ]
 
 
@@ -1015,3 +1016,26 @@ def dicts_equal(d1:dict, d2:dict) -> bool:
             if v != d2[k]:
                 return False
     return True
+
+
+def default_class_repr(c:object, align:str="center") -> str:
+    """ finished, checked,
+
+    Parameters
+    ----------
+    c: object,
+        the object to be represented
+    align: str, default "center",
+        the alignment of the class arguments
+    """
+    if hasattr(c, "extra_repr_keys") and len(c.extra_repr_keys()) > 0:
+        max_len = max([len(k) for k in c.extra_repr_keys()])
+        extra_str = "(\n" + \
+            ",\n".join([
+                f"""    {k.ljust(max_len, " ") if align.lower() in ["center", "c"] else k} = {v}""" \
+                    for k,v in c.__dict__.items() if k in c.extra_repr_keys()
+                ]) + \
+            "\n)"
+    else:
+        extra_str = ""
+    return f"{c.__class__.__name__}{extra_str}"
