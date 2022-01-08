8cf97aad1ccf35cae7baba440b5ce35f621d73df
WEN Hao <wenh06@gmail.com>
2022-01-08 16:57:51 +0800

    add CFG, to replace EasyDict

--- torch_ecg/cfg.py
+++ torch_ecg/cfg.py
@@ -2,13 +2,14 @@
 """
 
 import os
+from typing import Optional, MutableMapping, NoReturn
 
 import numpy as np
 import torch
 from easydict import EasyDict as ED
 
 
-__all__ = ["DEFAULTS"]
+__all__ = ["CFG", "DEFAULTS",]
 
 
 _PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -16,7 +17,34 @@ _PROJECT_CACHE = os.path.join(os.path.expanduser("~"), ".cache", "torch_ecg")
 os.makedirs(name=_PROJECT_CACHE, exist_ok=True)
 
 
-DEFAULTS = ED()
+class CFG(ED):
+    """
+    this class is created in order to renew the `update` method,
+    to fit the hierarchical structure of configurations
+
+    for example:
+    >>> c = CFG(hehe={"a":1,"b":2})
+    >>> c.update(hehe={"a":-1})
+    >>> c
+    ... {'hehe': {'a': -1, 'b': 2}}
+    """
+    __name__ = "CFG"
+
+    def update(self, new_cfg:Optional[MutableMapping]=None, **kwargs) -> NoReturn:
+        """
+        """
+        _new_cfg = new_cfg or CFG()
+        if len(kwargs) > 0:  # avoid RecursionError
+            _new_cfg.update(kwargs)
+        for k in _new_cfg:
+            # if _new_cfg[k].__class__.__name__ in ["dict", "EasyDict", "CFG"] and k in self:
+            if isinstance(_new_cfg[k], MutableMapping) and k in self:
+                self[k].update(_new_cfg[k])
+            else:
+                setattr(self, k, _new_cfg[k])
+
+
+DEFAULTS = CFG()
 
 DEFAULTS.log_dir = os.path.join(_PROJECT_CACHE, "log")
 DEFAULTS.checkpoints = os.path.join(_PROJECT_CACHE, "checkpoints")
