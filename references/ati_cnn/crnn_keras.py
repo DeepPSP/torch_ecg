7f0e18c4701a99ef4bb9c29ffb8a9af286699215
WEN Hao <wenh06@gmail.com>
2022-03-04 10:02:22 +0800

    remove the unnecessary reference folder

--- references/ati_cnn/crnn_keras.py
+++
@@ -1,6 +0,0 @@
-import argparse
-import numpy as np
-import keras
-from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
