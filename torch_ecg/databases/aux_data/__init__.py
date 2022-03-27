cf75b80f2441253048b1cca52844af251f5959a0
WEN Hao <wenh06@gmail.com>
2022-03-27 10:25:31 +0800

    add cached list of PhysioNet databases

---
+++ torch_ecg/databases/aux_data/__init__.py
@@ -0,0 +1,28 @@
+"""
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+from . import (
+    cinc2020_aux_data,
+    cinc2021_aux_data,
+)
+
+
+__all__ = [
+    "cinc2020_aux_data",
+    "cinc2021_aux_data",
+    "get_physionet_dbs",
+]
+
+
+def get_physionet_dbs() -> pd.DataFrame:
+    """
+    load the list of PhysioNet databases,
+    locally stored in the file "./physionet_dbs.csv.tar.gz"
+    """
+    return pd.read_csv(
+        Path(__file__).absolute().parent / "physionet_dbs.csv.tar.gz"
+    )
