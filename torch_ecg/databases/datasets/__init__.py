6cb273c257b062c9edf76ed2c2a4c468b4c7c072
WEN Hao <wenh06@gmail.com>
2022-03-30 11:42:28 +0800

    update __init__ of torch_ecg.databases.datasets module

---
+++ torch_ecg/databases/datasets/__init__.py
@@ -0,0 +1,27 @@
+"""
+"""
+
+# fmt: off
+from .cinc2020.cinc2020_cfg import CINC2020TrainCfg
+from .cinc2020.cinc2020_dataset import CINC2020Dataset
+from .cinc2021.cinc2021_cfg import CINC2021TrainCfg
+from .cinc2021.cinc2021_dataset import CINC2021Dataset
+from .cpsc2019.cpsc2019_cfg import CPSC2019TrainCfg
+from .cpsc2019.cpsc2019_dataset import CPSC2019Dataset
+# from .cpsc2020.cpsc2020_cfg import CPSC2020TrainCfg
+# from .cpsc2020.cpsc2020_dataset import CPSC2020Dataset
+from .cpsc2021.cpsc2021_cfg import CPSC2021TrainCfg
+from .cpsc2021.cpsc2021_dataset import CPSC2021Dataset
+from .ludb.ludb_cfg import LUDBTrainCfg
+from .ludb.ludb_dataset import LUDBDataset
+
+
+__all__ = [
+    "CINC2020TrainCfg", "CINC2020Dataset",
+    "CINC2021TrainCfg", "CINC2021Dataset",
+    "CPSC2019TrainCfg", "CPSC2019Dataset",
+    # "CPSC2020TrainCfg", "CPSC2020Dataset",
+    "CPSC2021TrainCfg", "CPSC2021Dataset",
+    "LUDBTrainCfg", "LUDBDataset",
+]
+# fmt: on
