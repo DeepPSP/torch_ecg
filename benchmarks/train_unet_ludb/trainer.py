correct the computation of the metric of mean_error for ECG wave delineation

benchmarks/train_unet_ludb/trainer.py
@@ -248,18 +248,19 @@ class LUDBTrainer(BaseTrainer):
 
         # TODO: provide numerical values for the metrics from all of the dicts of eval_res
         eval_res = {
-            metric: np.nanmean([eval_res_split[f"{wf}_{pos}"][metric]])
+            metric: np.nanmean(
+                [
+                    eval_res_split[f"{wf}_{pos}"][metric]
+                    for wf in self._cm
+                    for pos in ["onset", "offset"]
+                ]
+            )
             for metric in [
                 "sensitivity",
                 "precision",
                 "f1_score",
                 "mean_error",
                 "standard_deviation",
-            ]
-            for wf in self._cm
-            for pos in [
-                "onset",
-                "offset",
             ]
         }
 

torch_ecg/databases/physionet_databases/ludb.py
@@ -1191,7 +1191,7 @@ def _compute_metrics_base(
         _pred = _preds[np.where(np.abs(_preds - point) <= _tolerance)[0].tolist()]
         if len(_pred) > 0:
             truth_positive += 1
-            idx = np.argmin(np.abs(_preds - point))
+            idx = np.argmin(np.abs(_pred - point))
             errors.append(_pred[idx] - point)
         else:
             false_negative += 1