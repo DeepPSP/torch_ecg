correct the computation of the metric of mean_error for ECG wave delineation

benchmarks/train_unet_ludb/metrics.py
@@ -346,7 +346,8 @@ def _compute_metrics_base(
         _pred = _preds[np.where(np.abs(_preds - point) <= _tolerance)[0].tolist()]
         if len(_pred) > 0:
             truth_positive += 1
-            errors.append(_pred[0] - point)
+            idx = np.argmin(np.abs(_preds - point))
+            errors.append(_pred[idx] - point)
         else:
             false_negative += 1
         n_included += len(_pred)

benchmarks/train_unet_ludb/trainer.py
@@ -248,7 +248,7 @@ class LUDBTrainer(BaseTrainer):
 
         # TODO: provide numerical values for the metrics from all of the dicts of eval_res
         eval_res = {
-            metric: np.mean([eval_res_split[f"{wf}_{pos}"][metric]])
+            metric: np.nanmean([eval_res_split[f"{wf}_{pos}"][metric]])
             for metric in [
                 "sensitivity",
                 "precision",