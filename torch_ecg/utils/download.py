8f22086a8f09b438a3b2706f68945687eac00e62
WEN Hao <wenh06@gmail.com>
2022-11-03 17:58:04 +0800

    CVE-2007-4559 Patch

--- torch_ecg/utils/download.py
+++ torch_ecg/utils/download.py
@@ -15,7 +15,7 @@ import zipfile
 import warnings
 import urllib
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional, Union, Iterable
 
 import requests
 from tqdm.auto import tqdm
@@ -221,6 +221,65 @@ def _untar_file(path_to_tar_file: Union[str, Path], dst_dir: Union[str, Path]) -
     """
     print(f"Extracting file {path_to_tar_file} to {dst_dir}.")
     mode = Path(path_to_tar_file).suffix.replace(".", "r:").replace("tar", "")
-    # print(f"mode: {mode}")
     with tarfile.open(str(path_to_tar_file), mode) as tar_ref:
-        tar_ref.extractall(str(dst_dir))
+        # tar_ref.extractall(str(dst_dir))
+        # CVE-2007-4559:
+        # directory traversal vulnerability in `extract` and `extractall` in `tarfile` module
+        _safe_tar_extract(tar_ref, str(dst_dir))
+
+
+def _is_within_directory(directory: Union[str, Path], target: Union[str, Path]) -> bool:
+    """
+    check if the target is within the directory
+
+    Parameters
+    ----------
+    directory: str or Path,
+        path to the directory
+    target: str or Path,
+        path to the target
+
+    Returns
+    -------
+    bool,
+        True if the target is within the directory, False otherwise.
+
+    """
+    abs_directory = os.path.abspath(directory)
+    abs_target = os.path.abspath(target)
+
+    prefix = os.path.commonprefix([abs_directory, abs_target])
+
+    return prefix == abs_directory
+
+
+def _safe_tar_extract(
+    tar: tarfile.TarFile,
+    dst_dir: Union[str, Path],
+    members: Optional[Iterable[tarfile.TarInfo]] = None,
+    *,
+    numeric_owner: bool = False,
+) -> None:
+    """
+    Extract members from a tarfile **safely** to a destination directory.
+
+    Parameters
+    ----------
+    tar: tarfile.TarFile,
+        the tarfile to extract from
+    dst_dir: str or Path,
+        the destination directory
+    members: Iterable[tarfile.TarInfo], optional,
+        the members to extract,
+        if None, extract all members,
+        if not None, must be a subset of the list returned by `tar.getmembers()`
+    numeric_owner: bool, default False,
+        if True, only the numbers for user/group names are used and not the names.
+
+    """
+    for member in members or tar.getmembers():
+        member_path = os.path.join(dst_dir, member.name)
+        if not _is_within_directory(dst_dir, member_path):
+            raise Exception("Attempted Path Traversal in Tar File")
+
+    tar.extractall(dst_dir, members, numeric_owner=numeric_owner)
