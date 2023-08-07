a477707ce0a4e911a9b4143cc203c9c594e27e26
WEN Hao <wenh06@gmail.com>
2023-08-07 22:56:13 +0800

    update python version requirements: drop 3.6, add 3.11

--- setup.py
+++ setup.py
@@ -48,15 +48,15 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=install_requires,
     extras_require=extras,
 )
