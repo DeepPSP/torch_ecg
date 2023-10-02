ad8dbfee2176d560c422e69da786192aff7e2ad9
WEN Hao <wenh06@gmail.com>
2023-10-03 01:03:18 +0800

    remove out-dated files

--- formatting.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-black . --extend-exclude .ipynb -v
-flake8 . --count --ignore="E501 W503 E203 F841" --show-source --statistics --exclude=./.*,build,dist
--- push2pypi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python setup.py sdist bdist_wheel
-twine upload dist/*
-rm -rf build dist torch_ecg.egg-info
