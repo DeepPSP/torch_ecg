fix potential bugs related to data overflow for numpy preprocessors as reported in issue #12

torch_ecg/_preprocessors/base.py
@@ -9,6 +9,7 @@ import numpy as np
 from biosppy.signals.tools import filter_signal
 from scipy.ndimage import median_filter
 
+from ..cfg import DEFAULTS
 from ..utils.misc import ReprMixin, add_docstring
 from ..utils.utils_signal import butter_bandpass_filter
 
@@ -129,9 +130,9 @@ def preprocess_multi_lead_signal(
     ], f"multi-lead signal format `{sig_fmt}` not supported"
     if sig_fmt.lower() in ["channel_last", "lead_last"]:
         # might have a batch dimension at the first axis
-        filtered_ecg = np.moveaxis(raw_sig, -2, -1)
+        filtered_ecg = np.moveaxis(raw_sig, -2, -1).astype(DEFAULTS.np_dtype)
     else:
-        filtered_ecg = raw_sig
+        filtered_ecg = np.asarray(raw_sig, dtype=DEFAULTS.np_dtype)
 
     # remove baseline
     if bl_win:
@@ -229,7 +230,7 @@ def preprocess_single_lead_signal(
     e.g. :meth:`~torch_ecg.utils.butter_bandpass_filter`.
 
     """
-    filtered_ecg = np.asarray(raw_sig)
+    filtered_ecg = np.asarray(raw_sig, dtype=DEFAULTS.np_dtype)
     assert filtered_ecg.ndim == 1, "single-lead signal should be 1d array"
 
     # remove baseline

torch_ecg/_preprocessors/resample.py
@@ -6,6 +6,7 @@ from typing import Any, List, Optional, Tuple
 import numpy as np
 import scipy.signal as SS
 
+from ..cfg import DEFAULTS
 from .base import PreProcessor
 
 __all__ = [
@@ -68,10 +69,10 @@ class Resample(PreProcessor):
         """
         self._check_sig(sig)
         if self.fs is not None:
-            rsmp_sig = SS.resample_poly(sig, up=self.fs, down=fs, axis=-1)
+            rsmp_sig = SS.resample_poly(sig.astype(DEFAULTS.np_dtype), up=self.fs, down=fs, axis=-1)
             new_fs = self.fs
         else:  # self.siglen is not None
-            rsmp_sig = SS.resample(sig, num=self.siglen, axis=-1)
+            rsmp_sig = SS.resample(sig.astype(DEFAULTS.np_dtype), num=self.siglen, axis=-1)
             new_fs = int(round(self.siglen / sig.shape[-1] * fs))
         return rsmp_sig, new_fs
 